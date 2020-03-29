13d0b203635551618fb46edbf2314e4c6bd25d48
Khoi Nguyen <khoi@nguyen.me.uk>
2020-03-29 05:09:58 +0200

    [FILTER] Move environment list to separate file

---
+++ pandoc/environment_list.py
@@ -0,0 +1,82 @@
+environments = {
+    'Definition': {
+        'title': 'Definition',
+        'prefix': '\\faBook \\faStar',
+        'env': 'alertblock',
+    },
+    'Exercise': {
+        'title': 'Exercise',
+        'prefix': '\\faBook \\faPencil',
+        'env': 'block',
+    },
+    'Example': {
+        'title': 'Example',
+        'prefix': '\\faPencilSquare',
+        'env': 'exampleblock',
+    },
+    'Extension': {
+        'title': 'Extension',
+        'prefix': '\\faPlus',
+        'env': 'exampleblock',
+    },
+    'Formula': {
+        'title': 'Formula',
+        'prefix': '\\faSuperscript',
+        'env': 'alertblock',
+    },
+    'Hint': {
+        'title': 'Hint',
+        'prefix': '\\faLightbulbO',
+        'env': 'exampleblock',
+    },
+    'Objective': {
+        'title': 'Lesson Objective',
+        'prefix': '\\faList\\ \\faPencilSquare',
+        'env': 'block',
+    },
+    'Question': {
+        'title': 'Question',
+        'prefix': '\\faQuestionCircle',
+        'env': 'block',
+    },
+    'Question': {
+        'title': 'Question',
+        'prefix': '\\faQuestionCircle',
+        'env': 'block',
+    },
+    'Recall': {
+        'title': 'Recall',
+        'prefix': '\\faHistory',
+        'env': 'alertblock',
+    },
+    'Remark': {
+        'title': 'Remark',
+        'prefix': '\\faPencil',
+        'env': 'block',
+    },
+    'Remember': {
+        'title': 'Remember',
+        'prefix': '\\faDownload',
+        'env': 'alertblock',
+    },
+    'Solution': {
+        'title': 'Solution',
+        'prefix': '\\faPencilSquareO \\faCheck',
+        'env': 'exampleblock',
+    },
+    'Starter': {
+        'title': 'Starter',
+        'prefix': '\\faBook \\faPencil \\faClockO',
+        'env': 'block',
+    },
+    'Theorem': {
+        'title': 'Theorem',
+        'prefix': '\\faBook\ \\faStar',
+        'env': 'block',
+    },
+    'Vocabulary': {
+        'title': 'Vocabulary',
+        'prefix': '\\faLanguage',
+        'env': 'block',
+    },
+}
--- pandoc/environments.py
+++ pandoc/environments.py
@@ -1,4 +1,5 @@
 from pandocfilters import toJSONFilter, attributes, Math, Span, RawInline, RawBlock
+from environment_list import environments
 
 blatex = lambda x: RawBlock('latex', x)
 ilatex = lambda x: RawInline('latex', x)
@@ -7,91 +8,6 @@ answer = lambda x, c: [ilatex('\\answer[{}]{{'.format(c))] + x + [ilatex('}')]
 
 envcount = 1
 first_env = True
-
-# {{{
-environments = {
-    'Definition': {
-        'title': 'Definition',
-        'prefix': '\\faBook \\faStar',
-        'env': 'alertblock',
-    },
-    'Exercise': {
-        'title': 'Exercise',
-        'prefix': '\\faBook \\faPencil',
-        'env': 'block',
-    },
-    'Example': {
-        'title': 'Example',
-        'prefix': '\\faPencilSquare',
-        'env': 'exampleblock',
-    },
-    'Extension': {
-        'title': 'Extension',
-        'prefix': '\\faPlus',
-        'env': 'exampleblock',
-    },
-    'Formula': {
-        'title': 'Formula',
-        'prefix': '\\faSuperscript',
-        'env': 'alertblock',
-    },
-    'Hint': {
-        'title': 'Hint',
-        'prefix': '\\faLightbulbO',
-        'env': 'exampleblock',
-    },
-    'Objective': {
-        'title': 'Lesson Objective',
-        'prefix': '\\faList\\ \\faPencilSquare',
-        'env': 'block',
-    },
-    'Question': {
-        'title': 'Question',
-        'prefix': '\\faQuestionCircle',
-        'env': 'block',
-    },
-    'Question': {
-        'title': 'Question',
-        'prefix': '\\faQuestionCircle',
-        'env': 'block',
-    },
-    'Recall': {
-        'title': 'Recall',
-        'prefix': '\\faHistory',
-        'env': 'alertblock',
-    },
-    'Remark': {
-        'title': 'Remark',
-        'prefix': '\\faPencil',
-        'env': 'block',
-    },
-    'Remember': {
-        'title': 'Remember',
-        'prefix': '\\faDownload',
-        'env': 'alertblock',
-    },
-    'Solution': {
-        'title': 'Solution',
-        'prefix': '\\faPencilSquareO \\faCheck',
-        'env': 'exampleblock',
-    },
-    'Starter': {
-        'title': 'Starter',
-        'prefix': '\\faBook \\faPencil \\faClockO',
-        'env': 'block',
-    },
-    'Theorem': {
-        'title': 'Theorem',
-        'prefix': '\\faBook\ \\faStar',
-        'env': 'block',
-    },
-    'Vocabulary': {
-        'title': 'Vocabulary',
-        'prefix': '\\faLanguage',
-        'env': 'block',
-    },
-}
-# }}}
 
 def environment(ident, env, keyvals, contents, count):
     data = environments[env]
