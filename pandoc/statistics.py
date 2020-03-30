a8f1909cbcc11b171488422bd1dd13571549c8e9
Thomas Chambaud-Boudet <thomaschambaud-boudet@Thomass-MacBook-Pro-3.local>
2020-03-30 20:45:01 +0100

    [STATS] Display question in mean

--- pandoc/statistics.py
+++ pandoc/statistics.py
@@ -1,8 +1,11 @@
 def mean(number_list, frequencies = False):
     if frequencies:
+        # KHOI TODO (Calculate variable question)
         running_total = 0
         for i in range(0, len(number_list)):
             running_total += number_list[i] * frequencies[i]
-        return running_total / sum(frequencies)
+        answer = running_total / sum(frequencies)
     else:
-        return sum(number_list) / len(number_list)
+        question = ", ".join([str(n) for n in number_list])
+        answer = sum(number_list) / len(number_list)
+    return (question, str(answer))
