5927e0380c0cffb02483f994aa09f2f7943a5721
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-04 04:17:45 +0200

    [TESTS] Check sympy imports

--- pandoc/run_test.py
+++ pandoc/run_test.py
@@ -1,16 +1,23 @@
 import yaml
 import sys
 from algebra import *
+from analysis import *
+from stats import *
+from probability import *
+import sympy
 
-with open('tests/algebra.yaml') as file:
-    tests = yaml.load(file, Loader=yaml.FullLoader)
+paths = ['general', 'algebra']
 
-    for test in tests:
-        result = eval(test['cmd'])
-        if isinstance(result, tuple):
-            result = list(result)
-        if result != test['out']:
-            print('Failed test:', test['name'])
-            print('Output:', result)
-            print('Expected result:', test['out'])
-            sys.exit(1)
+for path in paths:
+    with open(f'tests/{path}.yaml') as file:
+        tests = yaml.load(file, Loader=yaml.FullLoader)
+
+        for test in tests:
+            result = eval(test['cmd'])
+            if isinstance(result, tuple):
+                result = list(result)
+            if result != test['out']:
+                print('Failed test:', test['name'])
+                print('Output:', result)
+                print('Expected result:', test['out'])
+                sys.exit(1)
