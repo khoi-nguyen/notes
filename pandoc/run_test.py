b158604495a2bdec0d07ccff180e2541be85c09a
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-04 04:01:14 +0200

    [TESTS] Make tests quieter

--- pandoc/run_test.py
+++ pandoc/run_test.py
@@ -2,17 +2,14 @@ import yaml
 import sys
 from algebra import *
 
-with open('../tests/algebra.yaml') as file:
+with open('tests/algebra.yaml') as file:
     tests = yaml.load(file, Loader=yaml.FullLoader)
 
     for test in tests:
         result = eval(test['cmd'])
         if isinstance(result, tuple):
             result = list(result)
-        passed = result == test['out']
-        if passed:
-            print('Passed test:', test['name'])
-        else:
+        if result != test['out']:
             print('Failed test:', test['name'])
             print('Output:', result)
             print('Expected result:', test['out'])
