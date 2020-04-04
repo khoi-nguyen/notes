d85d9321bacf9004ccff5902379c53180e586798
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-04 12:33:02 +0200

    [TESTS] Try all yaml files in tests folder

--- pandoc/run_test.py
+++ pandoc/run_test.py
@@ -1,15 +1,14 @@
 import yaml
 import sys
+from glob import glob
 from algebra import *
 from analysis import *
 from stats import *
 from probability import *
 import sympy
 
-paths = ['general', 'algebra']
-
-for path in paths:
-    with open(f'tests/{path}.yaml') as file:
+for path in glob('./tests/*.yaml'):
+    with open(path) as file:
         tests = yaml.load(file, Loader=yaml.FullLoader)
 
         for test in tests:
