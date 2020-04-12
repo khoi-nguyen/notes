31f2235ee866707c49eb051e6307ff7a79a76841
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-12 15:06:16 +0200

    [WEB] Change home page

--- src/Home.vue
+++ src/Home.vue
@@ -1,37 +1,18 @@
 <template lang="pug">
 div
-    h1 Welcome
+    h1 Welcome to MathsNuggets!
     p.lead.
-        Hello! My name is Khoi Nguyen and
-        I am doing a teacher training programme at the
-        #[a(href='https://ox.ac.uk') University of Oxford].
-        From August 2020 onwards, I will be teaching mathematics at the
-        #[a(href='https://kingscollegeschools.org/madrid/school/') British School of Madrid].
+        Hello and welcome to our website! We are two PGCE students
+        at the #[a(href="https://ox.ac.uk") University of Oxford]
+        about to embark on a career in teaching.
+        You will find here our #[router-link(to="/teaching") teaching resources].
     p.
-        Before teaching, I was a web developer
-        (which you probably can't tell from this website,
-        since I wasn't very good at it) and before that,
-        I was a research student at
-        #[a(href='https://imperial.ac.uk') Imperial College].
-        If you want to see what a bad thesis looks like, click
-        #[a(href='https://spiral.imperial.ac.uk/handle/10044/1/44081') here].
-    p.
-        My greatest pride naturally lies in something I'm not very good at.
-        I took part in the
-        #[a(href='https://www.youtube.com/watch?v=d12Dgfd09SI') 77#[sup th] Table Tennis Varsity Game]
-        as Oxford's fourth player
-        (or Cambridge's fifth if you're a glass-half-full type of person).
-    hr
-    h3 Contact me
-    dl
-        dt Email
-        dd khoi@nguyen.me.uk
-        dt Address
-        dd.
-            St Stephen's House#[br]
-            16, Marston St#[br]
-            Oxford OX4 1JX#[br]
-            United Kingdom
+        We use programming to automate
+        as much of the resource creating process as possible.
+        For example, exercises are solved by the computer to ensure
+        the answers are correct.
+        More information can be found on
+        #[router-link(to="/project") this page].
 </template>
 
 <script>
