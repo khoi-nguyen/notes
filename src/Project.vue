de0daa05087cce1ba4993a9278527de3e2de3011
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-12 15:06:50 +0200

    [WEB] Remove build image from project page

--- src/Project.vue
+++ src/Project.vue
@@ -1,8 +1,6 @@
 <template lang="pug">
 div
     h1 Our project
-    p
-        img(src="https://github.com/khoi-nguyen/teaching/workflows/Build/badge.svg")
     p.lead.
         Our aim is to allow mathematics teachers to create resources easily and reliably,
         and for these resources to be shared in the spirit of projects like Wikipedia.
