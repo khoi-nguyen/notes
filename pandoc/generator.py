09d558c1bdd9747d1712e462a09ddd5d3dafb900
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-12 21:18:06 +0200

    [STARTER] Exercise on standard form

--- pandoc/generator.py
+++ pandoc/generator.py
@@ -3,6 +3,12 @@ from random import randint
 
 import sympy as s
 
+def generate_stf():
+    """Standard form"""
+    x = randint(0, 100)
+    power = randint(-5, 5) - len(str(x))
+    return stf(str(x*10**power))
+
 def generate_equation():
     """Linear equation"""
     a = (-1)**randint(0, 1)*randint(1, 5)
