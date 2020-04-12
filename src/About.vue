9b7d5da3273423c394b8f0906b9e52c149cc8f2a
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-12 14:46:03 +0200

    [WEB] Extend profile

--- src/About.vue
+++ src/About.vue
@@ -1,24 +1,34 @@
 <template lang="pug">
 div
     h1 About
-    .row
+    .row.text-justify
         .col-sm
             h2 Khoi Nguyen
             h4 Developer
             p.
-                Hi! I'm the main developer of this project
+                Hi! I'm Khoi Nguyen but my pupils call me Mr Nuggets
+                because they find it easier to pronounce.
+                I'm the main developer of this project
                 and am in charge of the Python code
                 that generates the teaching resources.
             p.
                 I'm currently doing my PGCE at the
-                #[a(href='https://ox.ac.uk') University of Oxford]
-                and will teach next year at the
+                #[a(href='https://ox.ac.uk') University of Oxford].
+                From August 2020 onwards, I will be teaching mathematics at the
                 #[a(href='https://kingscollegeschools.org/madrid/school/') British School of Madrid].
-                As a mathematician, my specialties were harmonic analysis
-                and differential geometry.
+            p.
+                Before teaching, I was a web developer, and before that,
+                I was a research student at
+                #[a(href='https://imperial.ac.uk') Imperial College],
+                where I studied harmonic analysis on Lie groups.
+                If you want to see what bad thesis looks like, click
+                #[a(href='https://spiral.imperial.ac.uk/handle/10044/1/44081') here].
             p.
                 When I'm not teaching or working on this project,
                 I enjoy table tennis and playing the piano.
+                I took part in the
+                #[a(href='https://www.youtube.com/watch?v=d12Dgfd09SI') 2020 Table Tennis Varsity Game]
+                as Oxford's fourth player.
         .col-sm
             h2 Tom Chambaud
             h4 Resource Coordinator
