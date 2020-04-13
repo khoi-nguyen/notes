df317f60afb01e4182136489cce418f7b72ce041
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-13 03:24:32 +0200

    [STARTERS] Add levels to quadratic equations

--- pandoc/generator.py
+++ pandoc/generator.py
@@ -25,9 +25,31 @@ def generate_equation(level):
 
 def generate_quadratic_equation(level):
     """Quadratic equation"""
-    x1 = randint(-5, 5)
-    x2 = randint(-5, 5)
-    a = (-1)**randint(0, 1) * randint(1, 3)
     x = s.symbols('x')
-    eq = s.expand((a*x - x1)*(x - x2))
-    return ('Solve',) + equation(str(eq))
+    # x^2 = square number
+    if level < 3:
+        square_number = randint(0, 9)**2
+        n = (-1)**randint(0, 1) * randint(0, square_number)
+        lhs, rhs = x**2 + n, n + square_number
+    # a*x(x - x1) = 0
+    elif level == 4:
+        a = (-1)**randint(0, 1) * randint(1, 4)
+        c = randint(-9, 9)
+        lhs, rhs = s.expand(a * x * (x - c)), 0
+    # (a*x - x1)*(x - x2) = 0, with a = 1 for level 5, with rhs for >= 7
+    elif level < 8:
+        a = (-1)**randint(0, 1) * randint(1, 3) if level > 5 else 1
+        x1, x2 = randint(-5, 5), randint(-5, 5)
+        lhs, rhs = s.expand((a*x - x1)*(x - x2)), 0
+        if level == 7:
+            b, c = randint(-9, 9), randint(-9, 9)
+            lhs = lhs + b*x + c
+            rhs = b*x + c
+    # Only require that discriminant is nonnegative
+    else:
+        a = (-1)**randint(0, 1) * randint(1, 4)
+        b, Delta = randint(-9, 9), randint(0, 9)
+        d, e = randint(-9, 9), randint(-9, 9)
+        lhs = s.expand(a* (x - (-b + s.sqrt(Delta))/(2*a)) * (x - (-b - s.sqrt(Delta))/(2*a)) + d*x + e)
+        rhs = d*x + e
+    return ('Solve',) + equation(str(lhs), str(rhs))
