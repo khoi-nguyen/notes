c24c996f04a5d3cf0609d51a3a02bd680432be2f
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-14 18:32:38 +0200

    [PY] Only list markdown files that are ready

--- bin/data.py
+++ bin/data.py
@@ -17,14 +17,16 @@ for path in files:
     path = str(path)
     cmd = 'pandoc -s {} -t markdown --template=pandoc/meta.json'.format(path)
     json_string = subprocess.check_output(cmd.split(' '))
-    data.append({
-        'path': path,
-        'year': path.split('/')[0],
-        'meta': json.loads(json_string),
-        'pdf': path.replace('.md', '.pdf'),
-        'handout': path.replace('.md', '.handout.pdf'),
-        'modified': datetime.fromtimestamp(modified).strftime('%d %b %Y %H:%m'),
-    })
+    meta = json.loads(json_string)
+    if metadata.get('publish'):
+        data.append({
+            'path': path,
+            'year': path.split('/')[0],
+            'meta': meta,
+            'pdf': path.replace('.md', '.pdf'),
+            'handout': path.replace('.md', '.handout.pdf'),
+            'modified': datetime.fromtimestamp(modified).strftime('%d %b %Y %H:%m'),
+        })
 
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile, indent=True)
