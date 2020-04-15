1b39e3b44bfdae1f5fce2d59c64c031a55a3f569
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-15 22:07:13 +0200

    [VIDEO] Multiplying indices

---
+++ videos/indices.py
@@ -0,0 +1,31 @@
+from manimlib.imports import *
+
+class MultiplyIndices(Scene):
+    def construct(self):
+        question = TexMobject('2^4',  '\\times', '2^3', '=')
+        question.set_color_by_tex_to_color_map({'4': BLUE, '3': RED})
+        self.play(Write(question))
+        self.wait()
+
+        rhs = TexMobject('2^4',  '\\times', '2^3')
+        rhs.set_color_by_tex_to_color_map({'4': BLUE, '3': RED})
+        rhs.next_to(question, RIGHT)
+        self.play(Write(rhs))
+        self.wait()
+
+        equation = VGroup(question, rhs)
+        self.play(ApplyMethod(equation.to_edge, LEFT))
+        self.wait()
+
+        expanded_rhs = TexMobject('{2} \\times 2 \\times 2 \\times 2',  '\\times', '2 {\\times} 2 \\times 2')
+        expanded_rhs.set_color_by_tex('{2} ', BLUE)
+        expanded_rhs.set_color_by_tex('{\\times}', RED)
+        expanded_rhs.next_to(question, RIGHT)
+        self.play(Transform(rhs, expanded_rhs))
+        self.wait(3)
+
+        simplified_rhs = TexMobject('2^7', color=GREEN)
+        simplified_rhs.next_to(question, RIGHT)
+        self.play(Transform(rhs, simplified_rhs))
+
+        self.wait(5)
