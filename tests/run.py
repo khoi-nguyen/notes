515dc26e5dc02d5dcdc2121799bf068374cedc78
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-26 22:22:26 +0200

    [TESTS] Fix import due to pandoc refactoring

--- tests/run.py
+++ tests/run.py
@@ -6,7 +6,7 @@ from solver.analysis import *
 from solver.geometry import *
 from solver.stats import *
 from solver.probability import *
-from pandoc.pythontex import *
+from pandoc.filters.pythontex import *
 
 for path in glob('./tests/*.yaml'):
     with open(path) as file:
