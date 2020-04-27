be608c0833fe6b2fbf21de641bf2c5229425a382
Khoi Nguyen <khoi@nguyen.me.uk>
2020-04-27 22:27:10 +0200

    [GEOMETRY] Fix file syntax

--- solver/geometry.py
+++ solver/geometry.py
@@ -1,6 +1,7 @@
-import sympy as s
+from sympy import cos, latex, oo, pi, solveset, sympify
+from sympy import Interval, Symbol
 
-PI = s.pi
+PI = pi
 DEG = PI / 180
 RAD = 180 / PI
 
@@ -14,24 +15,24 @@ def cosine_law(a, b, c, gamma="gamma", radians=False):
     :param gamma: angle contained between the first two sides
     :param radians: use radians if True, degrees otherwise
     """
-    lengths = [s.sympify(l) for l in [a, b, c]]
+    lengths = [sympify(l) for l in [a, b, c]]
     [a, b, c] = lengths
-    is_symbol = [isinstance(l, s.Symbol) for l in lengths]
+    is_symbol = [isinstance(l, Symbol) for l in lengths]
 
     # Determine symbol, the sought quantity
     solve_for_angle = is_symbol == 3 * [False]
     if solve_for_angle:
-        gamma = s.Symbol(gamma)
+        gamma = Symbol(gamma)
         symbol = gamma
     else:
         symbol = lengths[is_symbol.index(True)]
 
-    dom = s.Interval(*(0, PI / 2 if solve_for_angle else s.oo))
-    sols = s.solveset(
-        a ** 2 + b ** 2 - 2 * a * b * s.cos(gamma) - c ** 2, symbol, domain=dom
+    dom = Interval(*(0, PI / 2 if solve_for_angle else oo))
+    sols = solveset(
+        a ** 2 + b ** 2 - 2 * a * b * cos(gamma) - c ** 2, symbol, domain=dom
     )
     sol = sols.args[0] * (1 if radians or not solve_for_angle else RAD)
 
-    exercise = f"{a}, {b}, {c}, {gamma}"
-    solution = f"{s.latex(symbol)} = {s.latex(sol)}"
+    exercise = f"{latex(a)}, {latex(b)}, {latex(c)}, {latex(gamma)}"
+    solution = f"{latex(symbol)} = {latex(sol)}"
     return (exercise, solution)
