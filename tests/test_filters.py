e3f27bd0a9a3f08d11aa4198b57e5664992caafb
Khoi Nguyen <khoi@nguyen.me.uk>
2020-05-02 02:44:20 +0200

    [TESTS] Use function to apply filter

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,24 +1,26 @@
 from panflute import convert_text, run_filter, stringify
 
 
+def apply_filter(function, markdown):
+    doc = convert_text(markdown, standalone=True)
+    return run_filter(function, doc=doc)
+
+
 def test_multicols():
     from pandoc.filters.multicols import multicols
 
     markdown = "\n".join(6 * ["#) Hello"])
-    doc = convert_text(markdown, standalone=True)
-    doc = run_filter(multicols, doc=doc)
+    doc = apply_filter(multicols, markdown)
     assert "multicols" in stringify(
         doc
     ), "Multicols in ordered lists with more than five elements"
 
     markdown = "::: {.Example cols=5}\nHello\n:::"
-    doc = convert_text(markdown, standalone=True)
-    doc = run_filter(multicols, doc=doc)
+    doc = apply_filter(multicols, markdown)
     assert "multicols}{5" in stringify(doc), "Multicols in div.env with cols attribute"
 
     markdown = "::: {.cols cols=3}\nHello\n:::"
-    doc = convert_text(markdown, standalone=True)
-    doc = run_filter(multicols, doc=doc)
+    doc = apply_filter(multicols, markdown)
     assert "multicols}{3" in stringify(doc), "Multicols in div.cols"
 
 
@@ -30,8 +32,7 @@ def test_environments():
     data = environments[env]
 
     markdown = f"::: {{.{env} t='{title}'}}\nHello\n:::"
-    doc = convert_text(markdown, standalone=True)
-    doc = run_filter(div_to_env, doc=doc)
+    doc = apply_filter(div_to_env, markdown)
 
     assert "colorenv" in stringify(
         doc
@@ -40,6 +41,5 @@ def test_environments():
     assert title in stringify(doc), "Honour the t attribute"
 
     markdown = "[1cm]{.gap}"
-    doc = convert_text(markdown, standalone=True)
-    doc = run_filter(div_to_env, doc=doc)
+    doc = apply_filter(div_to_env, markdown)
     assert "vspace" in stringify(doc), "Gap classes become vspaces"
