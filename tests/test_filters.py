0244607345d1f5b8d82b31a690727ecd560a4ef7
Khoi Nguyen <khoi@nguyen.me.uk>
2020-05-02 02:24:36 +0200

    [TESTS] Test environment filter

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -20,3 +20,26 @@ def test_multicols():
     doc = convert_text(markdown, standalone=True)
     doc = run_filter(multicols, doc=doc)
     assert "multicols}{3" in stringify(doc), "Multicols in div.cols"
+
+
+def test_environments():
+    from pandoc.filters.environments import div_to_env
+    from pandoc.filters.environment_list import environments
+
+    env, title = "Example", "Title"
+    data = environments[env]
+
+    markdown = f"::: {{.{env} t='{title}'}}\nHello\n:::"
+    doc = convert_text(markdown, standalone=True)
+    doc = run_filter(div_to_env, doc=doc)
+
+    assert "colorenv" in stringify(
+        doc
+    ), "Div with appropriate classes must become environments"
+    assert data["title"] in stringify(doc), "Ensure it has become the right environment"
+    assert title in stringify(doc), "Honour the t attribute"
+
+    markdown = "[1cm]{.gap}"
+    doc = convert_text(markdown, standalone=True)
+    doc = run_filter(div_to_env, doc=doc)
+    assert "vspace" in stringify(doc), "Gap classes become vspaces"
