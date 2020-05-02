16a0df70600362bf3ed090153ace498d4751558a
Khoi Nguyen <khoi@nguyen.me.uk>
2020-05-02 21:08:06 +0200

    [TESTS] Run appropriate command on test data

--- tests/test_maths.py
+++ tests/test_maths.py
@@ -3,13 +3,18 @@ from tests.algebra import tests as algebra_tests
 from tests.geometry import tests as geometry_tests
 
 
-@pytest.mark.parametrize(
-    "cmd, exercise, solution, title", algebra_tests,
-)
+tests = algebra_tests + geometry_tests
+
+
+def filter_by_len(tests, n):
+    return [t for t in tests if len(t) == n]
+
+
+@pytest.mark.parametrize("cmd, exercise, solution, title", filter_by_len(tests, 4))
 def test_command(cmd, exercise, solution, title):
     assert cmd == (exercise, solution), title
 
 
-@pytest.mark.parametrize("cmd, solution, title", geometry_tests)
+@pytest.mark.parametrize("cmd, solution, title", filter_by_len(tests, 3))
 def test_geometry(cmd, solution, title):
     assert cmd == solution, title
