e4b123dc948e0f6724f61f904a0c19a50e303c2c
Khoi Nguyen <khoi@nguyen.me.uk>
2020-05-02 21:01:59 +0200

    [TESTS] Move geometry tests to pytests

--- tests/test_maths.py
+++ tests/test_maths.py
@@ -1,5 +1,6 @@
 import pytest
 from tests.algebra import tests as algebra_tests
+from tests.geometry import tests as geometry_tests
 
 
 @pytest.mark.parametrize(
@@ -7,3 +8,8 @@ from tests.algebra import tests as algebra_tests
 )
 def test_command(cmd, exercise, solution, title):
     assert cmd == (exercise, solution), title
+
+
+@pytest.mark.parametrize("cmd, solution, title", geometry_tests)
+def test_geometry(cmd, solution, title):
+    assert cmd == solution, title
