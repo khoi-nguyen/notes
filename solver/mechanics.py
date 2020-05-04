20015635efb57f8958ee9a9adf13709f8a3e20e2
Khoi Nguyen <khoi@nguyen.me.uk>
2020-05-05 00:51:59 +0200

    [MECHANICS] Speed -> velocity

--- solver/mechanics.py
+++ solver/mechanics.py
@@ -5,9 +5,9 @@ from sympy import integrate, solve, symbols
 def motion(t0="t_0", x0="x_0", v0="v_0", T="T", X="x_T", V="v_T", a="a"):
     def solution(t0, x0, v0, T, X, V, a):
         s, t = symbols("s t")
-        speed = v0 + integrate(a.subs(t, s), (s, t0, t))
-        position = x0 + integrate(speed.subs(t, s), (s, t0, t))
-        equations = [position.subs(t, T) - X, speed.subs(t, T) - V]
+        velocity = v0 + integrate(a.subs(t, s), (s, t0, t))
+        position = x0 + integrate(velocity.subs(t, s), (s, t0, t))
+        equations = [position.subs(t, T) - X, velocity.subs(t, T) - V]
         return solve(equations)
 
     return Problem(solution)(t0, x0, v0, T, X, V, a)
